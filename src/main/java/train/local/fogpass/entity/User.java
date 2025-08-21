619009c6e15eaeb87268f8933c0dfd8d7fd9b0f5
trainee1-GT <trainee.1@greentinsolutions.com>
2025-08-21 14:22:26 +0530

    added fields in user table

--- src/main/java/train/local/fogpass/entity/User.java
+++ src/main/java/train/local/fogpass/entity/User.java
@@ -1,23 +1,74 @@
 package train.local.fogpass.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import java.util.HashSet;
+import java.util.Set;
 
-@Data
 @Entity
-@Table(name = "userlogin")
+@Table(name = "users", indexes = {
+        @Index(name = "idx_users_username", columnList = "username", unique = true)
+})
 public class User {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 50)
+    // Old fields
+    @Column(name = "User_Id", nullable = false, length = 50)
+    private String userId;
+
+    @Column(name = "User_Id", nullable = false, length = 50)
+
+    private String locoPilotId;
+
+    @Column(name = "user_name", nullable = false, unique = true, length = 50)
     private String username;
 
-    @Column(nullable = false, length = 255)
-    private String password;
+
+    @Column(name = "user_pwd", nullable = false, unique = true, length = 50)
+    private String pwd;
+
+    @Column(name = "Des", length = 100)
+    private String des;
+
+    @Column(name = "Dept", length = 100)
+    private String dept;
+
+    @Column(name = "BOD", length = 20)
+    private String bod;
+
+
+
+    // Inverse side of one-to-one
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    private UserSettings userSettings;
+
+    // Inverse side of user-access-scopes
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<UserAccessScope> accessScopes = new HashSet<>();
+
+    // Inverse side of journeys
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    private Set<Journey> journeys = new HashSet<>();
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
     public String getUsername() {
         return username;
@@ -27,13 +78,69 @@ public class User {
         this.username = username;
     }
 
-    public String getPassword() {
-        return password;
+    public String getPwd() {
+        return pwd;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getDes() {
+        return des;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+
+    public String getBod() {
+        return bod;
+    }
+
+    public void setBod(String bod) {
+        this.bod = bod;
     }
 
 
+
+    public String getLocoPilotId() {
+        return locoPilotId;
+    }
+
+    public void setLocoPilotId(String locoPilotId) {
+        this.locoPilotId = locoPilotId;
+    }
+
+    public UserSettings getUserSettings() {
+        return userSettings;
+    }
+
+    public void setUserSettings(UserSettings userSettings) {
+        this.userSettings = userSettings;
+    }
+
+    public Set<UserAccessScope> getAccessScopes() {
+        return accessScopes;
+    }
+
+    public void setAccessScopes(Set<UserAccessScope> accessScopes) {
+        this.accessScopes = accessScopes;
+    }
+
+    public Set<Journey> getJourneys() {
+        return journeys;
+    }
+
+    public void setJourneys(Set<Journey> journeys) {
+        this.journeys = journeys;
+    }
 }
